ba76be64230eec3295694cc19b63759069dcd61d
DOM HENG <dompozz21@gmail.com>
2024-07-26 11:31:08 +0700

    Add project files.

---
+++ Test_Git_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_Git_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
